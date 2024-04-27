41a471d9d860b4805b219328b8641c94c12b4362
Diego Cumplido <137076465+BODYSN4TCHER00@users.noreply.github.com>
2024-04-26 21:23:38 -0600

    Updated backend

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+
 const authRoutes = require('./routes/auth');
 const dispositivoRoutes = require('./routes/dispositivoRoutes');
 const userRoutes = require('./routes/userRoutes');
@@ -9,20 +13,42 @@ const historialRoutes = require('./routes/historialRoutes');
 
 const app = express();
 
-// Middlewares
-app.use(cors());
+// Middlewares de seguridad
+app.use(helmet());
+app.use(cors({
+  origin: 'https://sprout-af636.web.app/',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
+
+// Limitación de tasa de peticiones
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutos
+  max: 100
+});
+app.use(limiter);
+
+// Middleware de logueo
+app.use(morgan('combined'));
 
 // Conectar a MongoDB
 mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log('Conectado a MongoDB Atlas'))
 .catch((error) => console.error('Error al conectar con MongoDB:', error));
 
+
 // Rutas
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/dispositivos', dispositivoRoutes);
 app.use('/api/historial', historialRoutes);
+
+// Manejo global de errores
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Algo salió mal!');
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
